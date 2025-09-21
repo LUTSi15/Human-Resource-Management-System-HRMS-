38c2a9c8e5d5d8d6823dbe92cae2620525c9992b
Ahmad Kolid <ahmad.kholid@imocha.com.my>
2025-09-21 12:23:19 +0800

    Finish important function

--- src/main/java/com/kholid/hrms/service/LeaveService.java
+++ src/main/java/com/kholid/hrms/service/LeaveService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kholid.hrms.model.Department;
 import com.kholid.hrms.model.LeaveApplication;
 import com.kholid.hrms.model.User;
 import com.kholid.hrms.repo.LeaveRepository;
@@ -16,24 +17,53 @@ public class LeaveService {
     @Autowired
     private LeaveRepository leaveRepository;
 
-    public LeaveApplication createLeave(User user, LocalDate date, String leaveReason) {
+    public String createLeave(User user, LocalDate date, String leaveReason) {
         LeaveApplication leave = new LeaveApplication();
         leave.setUser(user);
         leave.setDate(date);
         leave.setLeaveReason(leaveReason);
-        leave.setStatus("APPLY");
+        leave.setStatus("Apply");
         leave.setStatusReason(null);
         leave.setAppealReason(null);
 
-        return leaveRepository.save(leave);
+        leaveRepository.save(leave);
+
+        return "Leave application successfully sent";
     }
 
     public List<LeaveApplication> getLeavesByUser(User user) {
         return leaveRepository.findByUser(user);
     }
 
+    public List<LeaveApplication> getLeavesByDepartment(Department department) {
+        return leaveRepository.findByUserDepartmentId(department.getId());
+    }
+
     public List<LeaveApplication> getAllLeaves() {
         return leaveRepository.findAll();
+    }
+
+    public String approveLeave(Long id) {
+        LeaveApplication leave = leaveRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid leave Id:" + id));
+        leave.setStatus("Approved");
+        leaveRepository.save(leave);
+        return "Successfully approved";
+    }
+
+    public String rejectLeave(Long id, String statusreason) {
+        LeaveApplication leave = leaveRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid leave Id:" + id));
+        leave.setStatus("Reject");
+        leave.setStatusReason(statusreason);
+        leaveRepository.save(leave);
+        return "Successfully reject";
+    }
+
+    public String appealReject(Long id, String appealReason) {
+        LeaveApplication leave = leaveRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid leave Id:" + id));
+        leave.setStatus("Appeal");
+        leave.setAppealReason(appealReason);
+        leaveRepository.save(leave);
+        return "Appeal Successfully sent";
     }
 }
 
